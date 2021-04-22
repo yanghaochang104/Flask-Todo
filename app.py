528d15ef6cd814f557688d2df51090a57ed9249b
ray.chang <yanghaochang1004@gmail.com>
2021-04-22 23:50:47 +0800

    feat(template): add template 'index.html'

--- app.py
+++ app.py
@@ -4,4 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=[{
+        'description': 'Todo 1'
+    }, {
+        'description': 'Todo 2'
+    }, {
+        'description': 'Todo 3'
+    }])

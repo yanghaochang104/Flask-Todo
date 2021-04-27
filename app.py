8cb8aa3387e5d368d3d12331b018109b166e9a0e
ray.chang <yanghaochang1004@gmail.com>
2021-04-27 17:55:25 +0800

    feat(migration): add flask-migration

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import keys
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = keys.DB_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 # setup schema
+
+
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
@@ -16,8 +21,6 @@ class Todo(db.Model):
     def __repr__(self):
         return f'Todo {self.id} {self.description}'
 
-# create table
-db.create_all()
 
 @app.route('/')
 def index():

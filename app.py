ae7ab5a9e1d59b9e3834af1a1214f90870e21985
ray.chang <yanghaochang1004@gmail.com>
2021-04-28 20:32:29 +0800

    feat(relationship): add TodoList table and migrate DB

--- app.py
+++ app.py
@@ -11,18 +11,21 @@ db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
 
-# setup schema
-
-
 class Todo(db.Model):
     __tablename__ = 'todos'
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'Todo {self.id} {self.description}'
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='todolists', lazy=True)
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
